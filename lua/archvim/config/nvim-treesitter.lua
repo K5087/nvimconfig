871b950a67e38dab1fe589f50c5c62e7c5938f0c
K5087 <18336485360@163.com>
2025-10-12 09:35:17 +0800

    修改treesitter“

--- lua/archvim/config/nvim-treesitter.lua
+++ lua/archvim/config/nvim-treesitter.lua
@@ -2,6 +2,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	event = "VeryLazy",
+    -- lazy = false,
 	branch = "main",
 	opts = {
 		ensure_installed = {
@@ -14,27 +15,31 @@ return {
 			"vim",
 			"vimdoc",
 		},
+		auto_install = true,
+		highlight = {
+			enable = true,
+		},
 	},
-	config = function(_, opts)
-		local nvim_treesitter = require("nvim-treesitter")
-		nvim_treesitter.setup()
-
-        local parsers = require("nvim-treesitter.parsers")
-		local pattern = {}
-		for _, parser in ipairs(opts.ensure_installed) do
-			local has_parser, _ = parsers[parser]
-			if not has_parser then
-				nvim_treesitter.install(parser)
-			else
-				vim.list_extend(pattern, vim.treesitter.language.get_filetypes(parser))
-			end
-		end
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = pattern,
-			callback = function()
-				vim.treesitter.start()
-			end,
-		})
-		vim.api.nvim_exec_autocmds("FileType", {})
-	end,
+	-- config = function(_, opts)
+	-- 	local nvim_treesitter = require("nvim-treesitter")
+	-- 	nvim_treesitter.setup()
+	--
+	-- 	-- local parsers = require("nvim-treesitter.parsers")
+	-- 	-- local pattern = {}
+	-- 	-- for _, parser in ipairs(opts.ensure_installed) do
+	-- 	-- 	local has_parser, _ = parsers[parser]
+	-- 	-- 	if not has_parser then
+	-- 	-- 		nvim_treesitter.install(parser)
+	-- 	-- 	else
+	-- 	-- 		vim.list_extend(pattern, vim.treesitter.language.get_filetypes(parser))
+	-- 	-- 	end
+	-- 	-- end
+	-- 	-- vim.api.nvim_create_autocmd("FileType", {
+	-- 	-- 	pattern = pattern,
+	-- 	-- 	callback = function()
+	-- 	-- 		vim.treesitter.start()
+	-- 	-- 	end,
+	-- 	-- })
+	-- 	-- vim.api.nvim_exec_autocmds("FileType", {})
+	-- end,
 }
