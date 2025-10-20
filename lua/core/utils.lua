84a2c20c0a8a8fe282942bb11bf713ebcb7a3376
moke <1635098863@qq.com>
2025-10-20 17:38:30 +0800

    重写cmake项目检查逻辑,仍需在linux平台验证

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -1,21 +1,5 @@
 local version = vim.fn.matchstr(vim.fn.execute("version"), "NVIM v\\zs[^\\n]*")
 
-local function check_cmake_project()
-	return vim.fn.findfile("CMakeLists.txt", ".;") ~= ""
-end
-
-local utils = {
-	version = version,
-
-	is_linux = vim.uv.os_uname().sysname == "Linux",
-	is_mac = vim.uv.os_uname().sysname == "Darwin",
-	is_windows = vim.uv.os_uname().sysname == "Windows_NT",
-	is_wsl = string.find(vim.uv.os_uname().release, "WSL") ~= nil,
-
-	is_cmake_project = check_cmake_project(),
-	is_window_suit = vim.fn.winwidth(0) > 80,
-}
-
 -- 为CMake项目创建一个命令组
 local cmake_group = vim.api.nvim_create_augroup("CMakeEvents", { clear = true })
 -- 为CMake项目创建一个事件
@@ -25,20 +9,35 @@ vim.api.nvim_create_autocmd({ "User" }, {
 	callback = function() end,
 })
 
+
+local utils = {
+	version = version,
+
+	is_linux = vim.uv.os_uname().sysname == "Linux",
+	is_mac = vim.uv.os_uname().sysname == "Darwin",
+	is_windows = vim.uv.os_uname().sysname == "Windows_NT",
+	is_wsl = string.find(vim.uv.os_uname().release, "WSL") ~= nil,
+
+	is_cmake_project = false,
+	is_window_suit = vim.fn.winwidth(0) > 80,
+}
+
+function utils:check_cmake_project()
+	local cmake_file = vim.fn.findfile("CMakeLists.txt", ".;")
+	utils.is_cmake_project = cmake_file ~= ""
+	if not utils.is_cmake_project then
+		return
+	end
+	local path = vim.fn.fnamemodify(cmake_file, ":p:h")
+	vim.api.nvim_exec_autocmds("User", { pattern = "CMakeProject", data = path })
+end
+
 -- 为Utils创建一个命令组
 local utils_group = vim.api.nvim_create_augroup("utils_upate", { clear = true })
 
 vim.api.nvim_create_autocmd("DirChanged", {
 	group = utils_group,
-	callback = function()
-		utils.is_cmake_project = check_cmake_project()
-		if not utils.is_cmake_project then
-			return
-		end
-		local cmake_list = vim.fn.findfile("CMakeLists.txt", ".;")
-		local path = vim.fn.fnamemodify(cmake_list, ":p:h")
-		vim.api.nvim_exec_autocmds("User", { pattern = "CMakeProject", data = path })
-	end,
+	callback = utils.check_cmake_project 
 })
 
 local suitwindow_update_event = { "VimResized", "BufEnter" }
