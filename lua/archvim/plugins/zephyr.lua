553f2315e606a9b2d03e3dd5e8d7d2d504a07d3a
moke <1635098863@qq.com>
2025-10-20 16:46:37 +0800

    修复lualine在dashboard中显示的问题

--- lua/archvim/plugins/zephyr.lua
+++ lua/archvim/plugins/zephyr.lua
@@ -3,19 +3,20 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		-- vim.cmd("colorscheme zephyr")
-		-- vim.cmd([[
-		--   hi Normal guibg=NONE ctermbg=NONE
-		--   hi NormalNC guibg=NONE ctermbg=NONE
-		--   hi VertSplit guibg=NONE
-		--   hi StatusLine guibg=NONE
-		-- ]])
-		vim.schedule(function()
-			vim.cmd("colorscheme zephyr")
-			vim.cmd([[
-		              hi Normal guibg=NONE ctermbg=NONE
-		              hi NormalNC guibg=NONE ctermbg=NONE
-		          ]])
-		end)
+		vim.cmd("colorscheme zephyr")
+		vim.cmd([[
+		  hi Normal guibg=NONE ctermbg=NONE
+		  hi NormalNC guibg=NONE ctermbg=NONE
+		  hi VertSplit guibg=NONE
+		  hi StatusLine guibg=NONE
+		]])
+        -- 这里的异步会导致lualine在第一次启动dashboard时错误
+		-- vim.schedule(function()
+		-- 	vim.cmd("colorscheme zephyr")
+		-- 	vim.cmd([[
+		--               hi Normal guibg=NONE ctermbg=NONE
+		--               hi NormalNC guibg=NONE ctermbg=NONE
+		--           ]])
+		-- end)
 	end,
 }
