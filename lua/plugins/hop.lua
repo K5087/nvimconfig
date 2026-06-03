41c758e75a449ada08ab2994c478da7f74b4e72d
moke <18336485360@163.com>
2026-06-03 11:58:22 +0800

    change hop shortcut

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -7,8 +7,8 @@ vim.api.nvim_create_autocmd("BufEnter", {
 	callback = function()
 		local hop = require("hop")
 		hop.setup({ keys = "etovxqpdygfblzhckisuran" })
-		vim.keymap.set({ "n", "v" }, "<leader><Space>", function()
+		vim.keymap.set({ "n", "v" }, "<leader>j", function()
 			hop.hint_char1({ current_line_only = false })
-		end, { remap = true })
+		end, { remap = true, desc = "hop jump" })
 	end,
 })
