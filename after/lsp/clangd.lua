29fd6532d7257902aa01456072f4a63eaca72f29
moke <18336485360@163.com>
2026-07-09 18:02:46 +0800

    change clangd setting

--- after/lsp/clangd.lua
+++ after/lsp/clangd.lua
@@ -1,5 +1,11 @@
 ---@type vim.lsp.Config
 return {
-    -- cmd = { "clangd", "--clang-tidy", "--header-insertion=never", "--completion-style=detailed" },
-    cmd = { "clangd", "--clang-tidy", "--header-insertion=never" }
+	-- cmd = { "clangd", "--clang-tidy", "--header-insertion=never", "--completion-style=detailed" },
+	cmd = {
+		"clangd",
+		"--clang-tidy", -- 进行一些额外检查和建议
+		"--background-index", -- 开启后台索引(用于避免每次打卡全量重建)
+		"--header-insertion=never", -- 不自动插入头文件
+		"--experimental-modules-support", -- 开启模块支持(在某些情况下clangd会构建自己的bmi进行索引)
+	},
 }
